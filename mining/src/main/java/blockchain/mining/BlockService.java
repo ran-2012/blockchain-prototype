d2db7bbafed02cb44502d7d074ca9e308690acec
yuanlonghang <eastin0621@gmail.com>
2023-11-14 17:11:37 +0800

    version2.0 mining

--- mining/src/main/java/blockchain/mining/BlockService.java
+++ mining/src/main/java/blockchain/mining/BlockService.java
@@ -9,9 +9,11 @@ import blockchain.data.core.Block;
 import blockchain.data.core.Transaction;
 import blockchain.data.core.TransactionInput;
 import blockchain.data.core.TransactionOutput;
+import blockchain.data.core.Utxo;
 import blockchain.network.INetwork;
 import blockchain.network.Network;
 import blockchain.storage.Storage;
+import blockchain.utility.Hash;
 import blockchain.utility.Log;
 
 import java.util.ArrayList;
@@ -21,7 +23,6 @@ import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-
 public class BlockService {
 
     Log log = Log.get(this);
@@ -73,21 +74,77 @@ public class BlockService {
      *
      * @return True: You can start mining now
      */
+
+
+    /**
+     * 现有问题：1.需要一个能获取当前block的函数，不是正在挖的，而是链上最后一个高度上面的block
+     *         2.交易池的意思是我从里面选交易来挖矿，如果挖矿成功了也存储成功了，那么区块里包括的交易就需要从交易池里删除；相反，revert的block
+     *         包括的交易也需要重新添加到交易池里面，是这样么？
+     *         3.utxo好像没list
+     *         4.******
+     * @return
+     */
     private boolean checkTransactionPool() {
-        // TODO: Check number of transaction in pool or time between previous block and current time.
+        // TODO: Check the time between previous block and current time.
+        long currentTime = new Date().getTime();
+        long previousBlockTime = ((Storage) Storage.getInstance()).getLastBlock().getTimestamp();
+        long timeDifference = currentTime - previousBlockTime;
+        if (timeDifference > 60000) {
+            return true;
+        }
         return false;
     }
 
-    private List<Transaction> pollTransaction() {
-        ArrayList list = new ArrayList<Transaction>();
-        // TODO: select transactions in the pool
+    private List<Transaction> poolTransaction() {
+        ArrayList<Transaction> list = new ArrayList<Transaction>();
+        //TODO: select valid transactions in the pool and add to the ArrayList list
+        List<Transaction> pool = poolTransaction.getInstance().getTransactions();
+        for (Transaction transaction : pool) {
+            if (isValidTransaction(transaction)) {
+                list.add(transaction);
+            }
+        }
         return list;
+    }
+
+    private boolean isValidTransaction(Transaction transaction) {
+        // TODO: Implement validation logic
+        // Check if inputs are valid
+        for (TransactionInput input : transaction.getInputs()) {
+            TransactionOutput output = Storage.getInstance().getTransactionOutput(input.getTransactionOutputId());
+            if (output == null) {
+                // The referenced output does not exist
+                return false;
+            }
+            if (input.getAmount() != output.getAmount()) {
+                // The input amount is not equal to the original output amount
+                return false;
+            }
+        }
+
+        // Check if outputs are valid
+        double inputSum = transaction.getInputs().stream().mapToDouble(TransactionInput::getAmount).sum();
+        double outputSum = transaction.getOutputs().stream().mapToDouble(TransactionOutput::getAmount).sum();
+        if (inputSum < outputSum) {
+            // The outputs exceed the inputs
+            return false;
+        }
+
+        // The transaction is valid
+        return true;
     }
 
     private Block generateNewBlock(List<Transaction> transactions) {
         // Generate block here then mine
         Block block = new Block();
-        // TODO: Fill data
+        //TODO:Fill related data into the new block
+        block.setPreviousHash(Storage.getInstance().getLastBlocks().get(0).getHash());
+        block.setTimestamp(new Date().getTime());
+        block.getTransactions().addAll(transactions);
+        //TODO:set nonce
+        block.setNonce(0);
+        block.setHeight(Storage.getInstance().getLastBlocks().get(0).getHeight() + 1); // Set the block height to the previous block's height plus one
+        block.setHash(Hash.hashString(block.toString()));
         return block;
     }
 
@@ -102,29 +159,73 @@ public class BlockService {
      */
     private class Internal {
         private void addNewBlock(Block block) {
-            // TODO: Check block; Add block to database, check if has multi branches
+            // TODO: Check block; Add block to database, check if the blockchain has multi branches in the previous height
+            // Check block
+            if (!block.getHash().equals(block.calculateHash())) {
+                log.error("Invalid block: hash mismatch");
+                return;
+            }
+            Block lastBlock = Storage.getInstance().getLastBlock();
+            if (!block.getPreviousHash().equals(lastBlock.getHash())) {
+                log.error("Invalid block: previous hash mismatch");
+                return;
+            }
+
+            // Add block to database
+            Storage.getInstance().addBlock(block);
+            // Check if the blockchain has multi branches in the previous height
+            List<Block> lastBlocks = Storage.getInstance().getLastBlocks();
+            if (lastBlocks.size() > 1) {
+                // Remove other blocks and only maintain the block with the smallest hash value
+                Block smallestBlock = lastBlocks.get(0);
+                for (Block b : lastBlocks) {
+                    if (b.getHash().compareTo(smallestBlock.getHash()) < 0) {
+                        smallestBlock = b;
+                    }
+                }
+                Storage.getInstance().removeBlocks(lastBlocks);
+                Storage.getInstance().addBlock(smallestBlock);
+            }
         }
 
         private void addNewTransaction(Transaction transaction) {
-            // TODO: Check Transaction; Add to pool
+            //TODO:Check Transaction and add the transcation of new block into the pool
+            if (isValidTransaction(transaction)) {
+                Storage.getInstance().addTransaction(transaction);
+            } else {
+                log.error("Invalid transaction: " + transaction);
+            }
         }
 
         private void revertBlock(String hash) {
             // TODO: Revert block, remove block data; add transaction and utxo back to the pool
-            // TODO: Blocks in previous height may also need to revert, remember to check them
-        }
-    }
+            // Delete block data
+            Storage.getInstance().removeBlockByHash(hash);
 
-    private final MiningService.Callback callback = new MiningService.Callback() {
-        @Override
-        public void onNewBlockMined(Block block) {
-            addNewBlock(block);
-            // TODO: Broadcast the block
+            // Add transactions back into the pool
+            Block block = Storage.getInstance().getBlock(hash);
+            if (block != null) {
+                List<Transaction> transactions = block.getTransactions();
+                for (Transaction transaction : transactions) {
+                    Storage.getInstance().addTransaction(transaction);
+                }
+
+                // TODO: Blocks in previous height may also need to revert, remember to check them
+            }
         }
 
+        private final MiningService.Callback callback = new MiningService.Callback() {
+            @Override
+            public void onNewBlockMined(Block block) {
+                addNewBlock(block);
+                //TODO: Broadcast the new block
+                INetwork network = Network.getInstance();
+                network.newBlock(block);
+            }
+        };
         @Override
         public void onAllNonceTried(Block block) {
-            // TODO: ues new timestamp then start again
+            // TODO: ues new timestamp then start mining again
             block.setTimestamp(new Date().getTime());
             miningService.setBlock(block);
             miningService.start();
@@ -143,7 +244,25 @@ public class BlockService {
         @Override
         public Transaction onNewTransactionRequested(String sourceAddress, String targetAddress, Long value) {
             // TODO: query utxo list and generate a transaction (or reject this transaction)
-            return super.onNewTransactionRequested(sourceAddress, targetAddress, value);
+            List<Utxo> utxoList = Storage.getInstance().getUTXOList(sourceAddress);
+            Long balance = 0L;
+            for (Utxo utxo : utxoList) {
+                balance += utxo.getValue();
+            }
+            if (balance < value) {
+                // Reject the transaction if the source address does not have enough funds
+                return null;
+            }
+
+            // Generate a new transaction
+            Transaction transaction = new Transaction(sourceAddress, targetAddress, value);
+            transaction.sign();
+
+            // Add the transaction to the transaction pool
+            addNewTransaction(transaction);
+
+            // Return the new transaction
+            return transaction;
         }
 
         @Override
